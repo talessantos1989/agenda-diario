d1f243a0583f0e77365e40301c9ff8355123d349
talessantos1989 <talesdealmeida@gmail.com>
2023-02-10 14:28:16 -0300

    imagems

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -5,7 +5,7 @@ function Banner(){
     return (
         <>
             <header className="banner-container">
-                <img className="banner-imagem" src="/imagens/banner-diario.png" alt="banner principal pagina organo"/>
+                <img className="banner-imagem" alt="banner principal pagina organo" src='../../imagens/banner-diario.png'/>
             </header>
             <h1 className="cabecalho-banner">Preencha com seu evento</h1>
         </>
